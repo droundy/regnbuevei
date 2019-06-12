4a591f5df29e374720ef2fbf1c892934f8040fce
David Roundy <roundyd@physics.oregonstate.edu>
2019-06-11 18:59:02 -0700

    update backdrop

--- three-color-manual/backdrop.py
+++ three-color-manual/backdrop.py
@@ -120,27 +120,27 @@ def bad_corners(s):
                     return True
     return False # no bad corners
 
-choices = np.linspace(0,1,4)
+choices = np.linspace(0,1,3)
 choices = np.append(choices, 0)
 choices = np.append(choices, 1)
 def choose(x):
-    x = [[choices[elem % 4] for elem in y] for y in x]
+    x = [[choices[elem % 3] for elem in y] for y in x]
     return np.array(x).T
 def choose2(x):
     x = np.array(x)
     return 
 
 scalars = choose([
-    [2  ,   3  , 2  , 3  , 0  ,   1  ],
-    [1  ,   3  , 2  , 3  , 1  ,   2  ],
-    [0  ,   3  , 2  , 3  , 2  ,   3  ],
-    [3  ,   3  , 2  , 3  , 1  ,   0  ],
-    [0  ,   1  , 2  , 3  , 1  ,   1  ],
-    [0  ,   1  , 2  , 3  , 1  ,   1  ],
-    [1  ,   1  , 2  , 3  , 1  ,   1  ],
-    [2  ,   1  , 2  , 3  , 1  ,   1  ],
-    [3  ,   1  , 2  , 3  , 1  ,   1  ],
-    [1  ,   1  , 2  , 3  , 1  ,   1  ],
+    [1  ,   2  , 1  , 2  , 0  ,   1  ],
+    [1  ,   2  , 1  , 2  , 1  ,   1  ],
+    [0  ,   2  , 1  , 2  , 1  ,   2  ],
+    [2  ,   2  , 1  , 2  , 1  ,   0  ],
+    [0  ,   1  , 1  , 2  , 1  ,   1  ],
+    [0  ,   1  , 1  , 2  , 1  ,   1  ],
+    [1  ,   1  , 1  , 2  , 1  ,   1  ],
+    [1  ,   1  , 1  , 2  , 1  ,   1  ],
+    [2  ,   1  , 1  , 2  , 1  ,   1  ],
+    [1  ,   1  , 1  , 2  , 1  ,   1  ],
 ])
 for ix in range(nx+1):
     for iy in range(ny+1):
